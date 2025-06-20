907463dd490352d384c60dc103f4ee91b738d878
Tigeystudios <tigeyonline@gmail.com>
2025-06-19 22:34:44 -0400

    Update home.py

--- home.py
+++ home.py
@@ -7,4 +7,44 @@ ACCOUNTS_FILE = 'accounts.json'
 with open(ACCOUNTS_FILE, 'r') as a:
 	accounts = json.load(a)
 
-st.title('Welcome to Chat Time Fun!') 
+def register_page():
+	new_un = st.text_input()
+	new_pw = st.text_input()
+	submitted = st.form_submit_button("Register")
+
+	if submitted:
+		accounts = st.session_state.accounts
+
+        if username in accounts:
+            st.error("Username already exists. Please choose a different one.")
+        if not username:
+            st.error("Username cannot be empty.")
+        if len(new_pw) < 6:
+            st.error("Password must be at least 6 characters long.")
+        if not re.search(r"[a-z]", new_pw):
+            st.error("Password must contain at least one lowercase letter.")
+        if not re.search(r"[A-Z]", new_pw):
+            st.error("Password must contain at least one uppercase letter.")
+        if not re.search(r"\d", new_pw):
+            st.error("Password must contain at least one digit.")
+        if not re.search(r"[!@#$%^&*()_+={}\[\]|\\:;'<>,.?/`~-]", new_pw):
+            st.error("Password must contain at least one special character.")
+
+def login_page():
+	pass
+
+def friend_select_page():
+	pass
+
+def text_friend_page():
+	pass
+
+def main():
+	reg = st.button('Register An Account')
+	if reg:
+		register_page()
+	log = st.button('Login')
+	if log:
+		login_page()
+
+main()
