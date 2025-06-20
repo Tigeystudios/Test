6c518d953471eb277eb764ee1f671bc85f4d2d46
Tigeystudios <tigeyonline@gmail.com>
2025-06-19 22:42:44 -0400

    Update home.py

--- home.py
+++ home.py
@@ -13,21 +13,20 @@ def register_page():
 	submitted = st.form_submit_button("Register")
 
 	if submitted:
-		accounts = st.session_state.accounts
-	        if username in accounts:
-	            st.error("Username already exists. Please choose a different one.")
-	        if not username:
-	            st.error("Username cannot be empty.")
-	        if len(new_pw) < 6:
-	            st.error("Password must be at least 6 characters long.")
-	        if not re.search(r"[a-z]", new_pw):
-	            st.error("Password must contain at least one lowercase letter.")
-	        if not re.search(r"[A-Z]", new_pw):
-	            st.error("Password must contain at least one uppercase letter.")
-	        if not re.search(r"\d", new_pw):
-	            st.error("Password must contain at least one digit.")
-	        if not re.search(r"[!@#$%^&*()_+={}\[\]|\\:;'<>,.?/`~-]", new_pw):
-	            st.error("Password must contain at least one special character.")
+		if username in accounts:
+	    st.error("Username already exists. Please choose a different one.")
+	  if not username:
+	    st.error("Username cannot be empty.")
+	  if len(new_pw) < 6:
+	    st.error("Password must be at least 6 characters long.")
+	  if not re.search(r"[a-z]", new_pw):
+	    st.error("Password must contain at least one lowercase letter.")
+	  if not re.search(r"[A-Z]", new_pw):
+	    st.error("Password must contain at least one uppercase letter.")
+	  if not re.search(r"\d", new_pw):
+	    st.error("Password must contain at least one digit.")
+	  if not re.search(r"[!@#$%^&*()_+={}\[\]|\\:;'<>,.?/`~-]", new_pw):
+	    st.error("Password must contain at least one special character.")
 
 def login_page():
 	pass
