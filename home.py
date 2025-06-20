4670855fa893567677deca4dbde5e322c45cbf45
Tigeystudios <tigeyonline@gmail.com>
2025-06-19 21:15:01 -0400

    Update home.py

--- home.py
+++ home.py
@@ -6,7 +6,7 @@ ACCOUNTS_FILE = 'accounts.json'
 try:
 	with open(ACCOUNTS_FILE, 'r') as a:
 		accounts = json.load(a)
-except JSONDecodeError:
+except json.JSONDecodeError:
 	st.warrning('There are no current accounts.')
 
 st.title('Welcome to Chat Time Fun!') 
